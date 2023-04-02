b91989a4b1544cce1d535dd65e36125688290714
Wagner Castro <wagner_gcastro@hotmail.com>
2023-04-02 06:35:17 -0300

    #03 - Resolved fix phpcs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,17 @@ var phpcs = require('gulp-phpcs');
 
 
 gulp.task('phpcs', function () {
-  return gulp.src(['public/index.php', 'src/**/*.php', '!src/index.php',  '!vendor/**/*.*', '!node_modules/**/*.*'])
+  return gulp.src([
+    'public/index.php',
+    'src/app/*.php',
+    'src/config/*.php',
+    'src/routes/*.php',
+    '!src/database/*.php',
+    '!src/index.php',
+    '!vendor/**/*.*',
+    '!node_modules/**/*.*'
+  ])
+
   // Validate files using PHP Code Sniffer
     .pipe(phpcs({
        bin: 'vendor/bin/phpcs',
@@ -14,23 +24,28 @@ gulp.task('phpcs', function () {
       // showSniffCode: 1,
       colors: 1
   }))
+
   // Log all problems that was found
     .pipe(phpcs.reporter('log'))
     .pipe(phpcs.reporter('file', { path: "code_sniffer_error.txt" }));
   });
 
 
-
 // Sass CSS e JS: Executa tudo em tempo real quando há alteração nos arquivos  em development
 gulp.task('watch', gulp.series( function() {
-  gulp.watch(['public/index.php', 'src/**/*.php'], gulp.parallel( ['phpcs']));
+  gulp.watch([
+    'public/index.php',
+    'src/app/*.php',
+    'src/config/*.php',
+    'src/routes/*.php'
+  ], gulp.parallel( ['phpcs']));
 }));
 
 
 /**
 * Defautl:
 *
-* 1 - Executar sempre quando há alterações em arquivos .css e .js
+* 1 - Executar sempre quando há alterações em arquivos .php
 *
 */
 gulp.task('default', gulp.series( ['watch'] ));
